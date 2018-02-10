0282d1966229a221706f1074c62c9c2a398016fa
Kiflom <33908508+kiflom2@users.noreply.github.com>
2018-02-09 16:39:01 -0800

    The version is changed from python 2.7  to 3.6

--- Checkers.py
+++ Checkers.py
@@ -17,120 +17,156 @@ brd = [
 
 ###################FUNCTIONS###############
 # Will print the checkers board
-def pretty_print(brd):
+
+#Generate board
+b = [['E', 'B','E','B','E','B','E','B'],
+     ['B','E','B','E','B','E','B','E'],
+     ['E','B','E','B','E','B','E','B'],
+     ['E','E','E','E','E','E','E','E'],
+     ['E','E','E','E','E','E','E','E'],
+     ['R','E','R','E','R','E','R','E'],
+     ['E','R','E','R','E','R','E','R'],
+     ['R','E','R','E','R','E','R','E']]
+
+def pretty_print(b):
     counter = 0
-    for row in brd:
+    for row in b:
         for cell in row:
             #print(counter,end=' ')
             print(cell, end='  ')
             if (counter == 7):
                 counter = 0
-                print("\n",end='')
+                print('\n',end='' '')
             else:
                 counter += 1
             #print(str(cell), end=' ')
 
 
+# possible moves
+def possible_moves(b, x, y):
+    if is_capture_move_r1_valid(b, x, y, x-1, y-1, x-2, y-2) == 'valid capturing move':
+        print(x-2, y-2)
+    if is_capture_move_r2_valid(b, x, y, x-1, y+1, x-2, y+2) == 'valid capturing move':
+        print(x-2, y+2)
+    if is_capture_move_b1_valid(b, x, y, x+1, y+1, x+2, y+2) == 'valid capturing move':
+        print(x+2, y+2)
+    if is_capture_move_b2_valid (b, x, y, x+1, y-1, x+2, y-2) == 'valid capturing move':
+        print(x+2, y-2)
+    if is_valid_move(b, x, y, x-1, y-1) == 'valid move':
+        print(x-1, y-1)
+    if is_valid_move(b, x, y, x-1, y+1) == 'valid move':
+        print(x-1, y+1)
+    if is_valid_move(b, x, y, x+1, y-1) == 'valid move':
+        print(x+1, y-1)
+    if is_valid_move(b, x, y, x+1, y+1) == 'valid move':
+        print(x+1, y+1)
 
-#TODO:incorporate a test to verify these functions
-# prints 'valid move' if the move is valid
-def is_valid_move(brd,(x, y),(x2, y2)):
-    if 0<=x<=7 and 0<=x2<=7 and 0<=y<=7 and 0<=y2<=7:
-        if brd[x2][y2] == 0: # is the destination space free
-            if brd[x][y] =='R':   #for the Red piece
-                if x2==x-1:
-                    if y2==y-1 or y2==y+1:
-                        print ('valid move') # move is valid
+#Legal lmoves
+def is_valid_move(b, x, y, x2, y2):
+    if 0 <= x <= 7 and 0 <= x2 <= 7  and 0 <= y <= 7 and 0 <= y2 <= 7:
+        if b[x2][y2] == 'E':  # is the destination space free
+            if b[x][y] == 'R':   #for the Red piece
+                if x2 == x-1:
+                    if y2 == y-1 or y2 == y+1:
+                        return 'valid move'  # move is valid
+                    else:
+                        return 'invalid move'
+                else:
+                    return 'invalid move'
 
-            elif brd[x][y]=='B': # for the Black piece
-                if x2==x+1:
-                    if y2==y-1 or y2== y+1:
-                        print ('valid move')  #move is valid
-    else:
-        print ('invalid move')
+            elif b[x][y] == 'B':  # for the Black piece
+                if x2 == x+1:
+                    if y2 == y-1 or y2 == y+1:
+                        return 'valid move'  #move is valid
+                    else:
+                        return 'invalid move'
+                else:
+                    return 'invalid move'
 
-# moves the piece; considering the move is valid
-#TODO:Error throwing and handling would be ideal here
-def move_piece(brd, (x, y), (x2, y2)):
-    brd[x][y], brd[x2][y2] = brd[x2][y2], brd[x][y]
-
-
-#TODO:This is an ideal place to use help functions this is long and very hard
-#to fix if something were to go wrong
-
-# carry out capturing move.
-def capturing_move(brd, (x1, y1), (x3, y3), (x2, y2)):
-    if brd[x1][y1] == 'R': # if it is R's turn
-        if brd[x2][y2] == brd[x1-2][y1-2] == 0:  # if the destination space is free
-            if brd[x3][y3] == brd[x1-1][y1-1] =='B':  # Situation 1
-                brd[x3][y3] = 0 # cancel B and change it by 0
-                brd[x1][y1], brd[x2][y2] = brd[x2][y2],brd[x1][y1] # exchange between R and empty space
-        elif brd[x2][y2] == brd[x1-2][y1+2] == 0:
-            if brd[x3][y3] == brd[x1-1][y1+1] == 'B': # Situation 2
-                brd[x3][y3] = 0 # cancel B and change it by 0
-                brd[x1][y1], brd[x2][y2] = brd[x2][y2],brd[x1][y1] # exchange between R and empty space
-    elif brd[x1][y1] == 'B': # if it is B's turn
-        if brd[x2][y2] == brd[x1+2][y1+2] == 0:  # if the destination space is free
-            if brd[x3][y3] == brd[x1+1][y1+1] =='R':  # situation 1
-                brd[x3][y3]=0  # cancel R and change it by 0
-                brd[x1][y1], brd[x2][y2] = brd[x2][y2],brd[x1][y1] # exchange between B and empty space
-        elif brd[x2][y2] == brd[x1+2][y1-2] == 0:
-            if brd[x3][y3] == brd[x1+1][y1-1] =='R': # situation 1
-                brd[x3][y3]=0  # cancel R and change it by 0
-                brd[x1][y1], brd[x2][y2] = brd[x2][y2],brd[x1][y1] # exchange between B and empty space
-
-
-
-#TODO: works great for top pieces but not for bottom pieces
-#print(list_possible_moves(brd,(2,1))) works great got (3,0) (3,2)
-#print(list_possible_moves(brd,(5,0))) NOT Working got (4,1) (4,-1)
-# prints possible available moves for (x, y)
-def list_possible_moves(brd, (x, y)):
-    # add how to avoid outputs which are out of the range (x-2)(y+2),(y-2),(x+2), (x-1), (y+1), (y-1), ,(x+1),
-    if brd[x][y]=='R':
-        if brd[x-1][y+1] =='B' and brd[x-1][y-1] =='B':
-            if brd[x-2][y+2]==0 and brd[x-2][y-2]==0:
-                print ((x-2, y+2), (x-2, y-2))
-            elif brd[x-2][y+2]==0 and brd[x-2][y-2]!=0:
-                print (x-2, y+2)
-            elif brd[x-2][y+2]!=0 and brd[x-2][y-2]==0:
-                print (x-2, y-2)
-        elif brd[x-1][y+1] =='B' and brd[x-1][y-1] !='B':
-            if brd[x-2][y+2]==0:
-                print (x-2, y+2)
-        elif brd[x-1][y+1] !='B' and brd[x-1][y-1] =='B':
-            if brd[x-2][y-2]==0:
-                print (x-2, y-2)
-        elif brd[x-1][y+1] ==0 and brd[x-1][y-1] ==0:
-            print ((x-1, y+1), (x-1, y-1))
-        elif brd[x-1][y+1] ==0 and brd[x-1][y-1] !=0:
-            print (x-1, y+1)
-        elif brd[x-1][y+1] !=0 and brd[x-1][y-1] ==0:
-            print (x-1, y-1)
         else:
-            print ('No available moves')
-    elif brd[x][y]=='B':
-        if brd[x+1][y-1]=='R' and brd[x+1][y+1]=='R':
-            if brd[x+2][y-2]==0 and brd[x+2][y+2] ==0:
-                print ((x+2, y-2), (x+2, y+2))
-            elif brd[x+2][y-2]==0 and brd[x+2][y+2] !=0:
-                print (x+2, y-2)
-            elif brd[x+2][y-2]!=0 and brd[x+2][y+2] ==0:
-                print (x+2, y+2)
-        elif brd[x+1][y-1]=='R' and brd[x+1][y+1]!='R':
-            if brd[x+2][y-2]==0:
-                print(x+2, y-2)
-        elif brd[x+1][y-1]!='R' and brd[x+1][y+1]=='R':
-            if brd[x+2][y+2] ==0:
-                print (x+2, y+2)
-        elif brd[x+1][y-1]==0 and brd[x+1][y+1]==0:
-            print ((x+1, y-1), (x+1, y+1))
-        elif brd[x+1][y-1]==0 and brd[x+1][y+1]!=0:
-            print (x+1, y-1)
-        elif brd[x+1][y-1]!=0 and brd[x+1][y+1]==0:
-            print (x+1, y+1)
-        else:
-            print ('No available moves')
+            return 'invalid move'
     else:
-        print ('No available moves')
+        return 'invalid move'
+
+#Test legal moves
+import unittest
+class Testlegalmoves(unittest.TestCase):
+    def test_is_valid_move(self):
+        self.assertEqual(legalmoves.is_valid_move(b, 2, 3, 3, 4), 'valid move')
+        self.assertEqual(legalmoves.is_valid_move(b, 5, 2, 4, 1), 'valid move')
+        self.assertEqual(legalmoves.is_valid_move(b, 6, 1, 5, 0), 'invalid move')
+        self.assertEqual(legalmoves.is_valid_move(b, 5, 2, 4, 2), 'invalid move')
+        self.assertEqual(legalmoves.is_valid_move(b, 6, 7, 5, 8), 'invalid move')
+
+
+#if __name__ == '__main__':
+    unittest.main()
+
+# Move a piece with error handling and throwing
+def move_piece(b, x, y, x2, y2):  # it couldnt catche when i put 6 variables instead of 5 ???
+    try:
+        b[x][y], b[x2][y2] = b[x2][y2], b[x][y]
+
+    except TypeError as e:
+        return 'Invalid input:' + str(e)
+
+    except IndexError as e:
+        return 'Out of range input:' + str(e)
+
+    except Exception as e:
+        return e
+
+
+# verifies capturing move
+def is_capture_move_r1_valid(b, x1, y1, x3, y3, x2, y2):
+    if 0 <= x1 <= 7 and 0 <= x2 <= 7 and 0 <= x3 <= 7 and 0 <= y1 <= 7 and 0 <= y2 <= 7 and 0 <= y3 <= 7:
+        if b[x1][y1] == 'R':  # if it is R's turn
+            if b[x2][y2] == b[x1 - 2][y1 - 2] == 'E' and b[x3][y3] == b[x1 - 1][y1 - 1] == 'B':  # if destspace is free
+                return 'valid capturing move'
+            else:
+                return 'invalid capturing move'
+    else:
+        return 'invalid capturing move'
+
+
+def is_capture_move_r2_valid(b, x1, y1, x3, y3, x2, y2):
+    if 0 <= x1 >= 7 and 0 <= x2 >= 7 and o <= x3 >= 7 and 0 <= y1 >= 7 and 0 <= y2 >= 7 and 0 <= y3 >= 7:
+        if b[x1][y1] == 'R':  # if it is R's turn
+            if b[x2][y2] == b[x1 - 2][y1 + 2] == 'E' and b[x3][y3] == b[x1 - 1][y1 + 1] == 'B':  # if dest space is free
+                return 'valid capturing move'
+            else:
+                return 'invalid capturing move'
+    else:
+        return 'invalid capturing move'
+
+
+def is_capture_move_b1_valid(b, x1, y1, x3, y3, x2, y2):
+    if 0 <= x1 >= 7 and 0 <= x2 >= 7 and o <= x3 >= 7 and 0 <= y1 >= 7 and 0 <= y2 >= 7 and 0 <= y3 >= 7:
+        if b[x1][y1] == 'B':  # if it is B's turn
+            if b[x2][y2] == b[x1 + 2][y1 + 2] == 'E' and b[x3][y3] == b[x1 + 1][y1 + 1] == 'R':  # if dest.space is free
+                return 'valid capturing move'
+            else:
+                return 'invalid capturing move'
+    else:
+        return 'invalid capturing move'
+
+
+
+def is_capture_move_b2_valid(b, x1, y1, x3, y3, x2, y2):
+    if 0 <= x1 >= 7 and 0 <= x2 >= 7 and o <= x3 >= 7 and 0 <= y1 >= 7 and 0 <= y2 >= 7 and 0 <= y3 >= 7:
+        if b[x1][y1] == 'B':  # if it is B's turn
+            if b[x2][y2] == b[x1 + 2][y1 - 2] == 'E' and b[x3][y3] == b[x1 + 1][y1 - 1] == 'R':  # if dest. space free
+                return 'valid capturing move'
+            else:
+                return 'invalid capturing move'
+    else:
+        return 'invalid capturing move'
+
+
+# make capturing move,  considering the move is legal
+def capturring_move(b, x1, y1, x3, y3, x2, y2):
+    b[x3][y3] = 'E'
+    b[x1][y1], b[x2][y2] = b[x2][y2], b[x1][y1]
+    pretty_print(b)  # why this became necessary??
+
 
